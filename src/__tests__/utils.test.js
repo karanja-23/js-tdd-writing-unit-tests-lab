6a8a9a298e3ab83ff69a4552ee49c41044e78f98
Hosea <karanja.mungai23@gmail.com>
2024-11-19 07:21:39 +0300

    modified tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,16 @@
 // Your tests here
+import { isPalindrome } from "../utils.js";
+describe("isPalindrome", () => {
+    it("should return true if the word is a palindrome", () => {
+        expect(isPalindrome("racecar")).toBe(true);
+    });
+    it("should return true if the word is a palindrome  butcase insensitive", () => {
+        expect(isPalindrome("RaCecAr")).toBe(true);
+    });    
+    it("should return false if the word is not a palindrome", () => {
+        expect(isPalindrome("hello")).toBe(false);
+    });
+    it ("should return false if the word is an empty string", () => {
+        expect(isPalindrome("")).toBe(false);
+    });
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+function isPalindrome(word) {
+    if (word === "") {
+        return false
+    }   
+    else if (word !== "" && word.toLowerCase() === word.toLowerCase().split("").reverse().join("")) {
+       return true
+   }
+   else {
+       return false
+   }
+}
+export { isPalindrome}
